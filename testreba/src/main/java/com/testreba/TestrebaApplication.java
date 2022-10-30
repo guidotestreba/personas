65c9949aaa36529e28e4478cea2cc1bc1a3cca99
guidotestreba <guido.test.reba@gmail.com>
2022-10-30 11:27:54 -0300

    commit proyecto

---
+++ testreba/src/main/java/com/testreba/TestrebaApplication.java
@@ -0,0 +1,13 @@
+package com.testreba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrebaApplication.class, args);
+	}
+
+}
---
+++ testreba/src/test/java/com/testreba/TestrebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testreba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
